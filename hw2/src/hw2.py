cd509d8a21eff772589267586ed733d16a5dfee2
alan910127 <alan469357@gmail.com>
2023-03-19 23:09:29 +0800

    ✨ Finish functionality

--- hw2/src/hw2.py
+++ hw2/src/hw2.py
@@ -1,20 +1,41 @@
+from __future__ import annotations
+
 import logging
 from dataclasses import dataclass, field
-from typing import TYPE_CHECKING, Protocol
+from enum import Enum, auto
+from typing import Optional, Protocol
 
 import setting
+from typing_extensions import Self
 
-if TYPE_CHECKING:
-    from typing import Optional, Self
-
-    Ip = str
-    Mac = str
-    Port = int
+IpAddress = str
+MacAddress = str
+PortNumber = int
 
 BROADCAST_MAC = "ffff"
 
+logger = logging.getLogger(__name__)
+
+
+class PacketType(Enum):
+    ARP = auto()
+    ICMP_REQUEST = auto()
+    ICMP_REPLY = auto()
+
+
+@dataclass
+class Packet:
+    type: PacketType
+    source_name: str
+    source_ip: IpAddress
+    source_mac: MacAddress
+    destination_ip: IpAddress
+    destination_mac: MacAddress
+
 
 class NetworkDevice(Protocol):
+    name: str
+
     def add(self, node: Self) -> None:
         """Link with another network device"""
 
@@ -24,17 +45,17 @@ class NetworkDevice(Protocol):
     def clear(self):
         """Clear the ARP or MAC table"""
 
-    def handle_packet(self, dest_mac: str):
+    def handle_packet(self, packet: Packet):
         """Handle the incoming packet"""
 
 
 @dataclass
 class Host:
     name: str
-    ip: Ip
-    mac: Mac
+    ip: IpAddress
+    mac: MacAddress
     port_to: Optional[NetworkDevice] = None
-    arp_table: dict[Ip, Mac] = field(default_factory=dict)
+    arp_table: dict[IpAddress, MacAddress] = field(default_factory=dict)
 
     def add(self, node: NetworkDevice):
         self.port_to = node
@@ -53,33 +74,96 @@ class Host:
 
         self.arp_table.clear()
 
-    def update_arp(self, ip: Ip, mac: Mac):
+    def update_arp(self, ip: IpAddress, mac: MacAddress):
         """Update ARP table with a new entry"""
 
         self.arp_table[ip] = mac
 
-    def handle_packet(self, dest_mac: Mac):
+    def handle_packet(self, packet: Packet):
         """Handle incoming packets"""
-        # TODO: handle packet
 
-    def ping(self, dest_ip: Ip):
-        """Handle a ping request"""
-        # TODO: handle ping
+        if packet.destination_mac not in (self.mac, BROADCAST_MAC):
+            return
 
-    def send(self):
+        if packet.type == PacketType.ICMP_REPLY:
+            return
+
+        if packet.type == PacketType.ARP:
+            # update the ARP table
+            self.update_arp(packet.source_ip, packet.source_mac)
+
+            if (
+                packet.destination_mac != BROADCAST_MAC
+                or packet.destination_ip != self.ip
+            ):
+                return
+
+            # send ARP reply to the source
+            arp_packet = Packet(
+                type=PacketType.ARP,
+                source_name=self.name,
+                source_ip=self.ip,
+                source_mac=self.mac,
+                destination_ip=packet.source_ip,
+                destination_mac=packet.source_mac,
+            )
+            return self.send(arp_packet)
+
+        # this is an ICMP request, send an ICMP reply to the source
+        icmp_packet = Packet(
+            type=PacketType.ICMP_REPLY,
+            source_name=self.name,
+            source_ip=self.ip,
+            source_mac=self.mac,
+            destination_ip=packet.source_ip,
+            destination_mac=packet.source_mac,
+        )
+        self.send(icmp_packet)
+
+    def ping(self, dest_ip: IpAddress):
+        """Send a ping request"""
+
+        if dest_ip not in self.arp_table:
+            # send an ARP request
+            arp_packet = Packet(
+                type=PacketType.ARP,
+                source_name=self.name,
+                source_ip=self.ip,
+                source_mac=self.mac,
+                destination_ip=dest_ip,
+                destination_mac=BROADCAST_MAC,
+            )
+            self.send(arp_packet)
+
+        dest_mac = self.arp_table[dest_ip]
+
+        # send an ICMP request to the destination
+        icmp_packet = Packet(
+            type=PacketType.ICMP_REQUEST,
+            source_name=self.name,
+            source_ip=self.ip,
+            source_mac=self.mac,
+            destination_ip=dest_ip,
+            destination_mac=dest_mac,
+        )
+        self.send(icmp_packet)
+
+    def send(self, packet: Packet):
         if self.port_to is None:
             logging.error(f"Host {self.name} has no port to send packets")
             return
 
-        # TODO: fill in parameters
-        self.port_to.handle_packet("")
+        logging.debug(
+            f"{self.name} sends an {packet.type.name} packet to {packet.destination_ip}"
+        )
+
+        self.port_to.handle_packet(packet)
 
 
 @dataclass
 class Switch:
     name: str
-    num_ports: int
-    mac_table: dict[Mac, Port] = field(default_factory=dict)
+    mac_table: dict[MacAddress, PortNumber] = field(default_factory=dict)
     port_to: list[NetworkDevice] = field(default_factory=list)
 
     def add(self, node: NetworkDevice):
@@ -99,20 +183,45 @@ class Switch:
 
         self.mac_table.clear()
 
-    def update_mac(self, mac_address: Mac, from_port: Port):
+    def update_mac(self, mac_address: MacAddress, from_port: PortNumber):
         """Update MAC table with a new entry"""
 
         self.mac_table[mac_address] = from_port
 
-    def handle_packet(self, dest_mac: Mac):
-        """Handle incoming packets"""
-        # TODO: handle packet
+    def get_port_by_name(self, name: str):
+        return next(i for i, node in enumerate(self.port_to) if node.name == name)
 
-    def send(self, index: Port):
+    def send_to_ports(self, packet: Packet, exclude: PortNumber):
+        for i, _ in enumerate(self.port_to):
+            if i == exclude:
+                continue
+
+            self.send(i, packet)
+
+    def handle_packet(self, packet: Packet):
+        """Handle incoming packets"""
+
+        source_port = self.get_port_by_name(packet.source_name)
+
+        self.update_mac(packet.source_mac, source_port)
+
+        if packet.type == PacketType.ARP:
+            # broadcast the packet
+            return self.send_to_ports(packet, exclude=source_port)
+
+        destination_port = self.mac_table.get(packet.destination_mac)
+
+        if destination_port is None:
+            # flood the packet
+            return self.send_to_ports(packet, exclude=source_port)
+
+        self.send(destination_port, packet)
+
+    def send(self, index: PortNumber, packet: Packet):
         """Send to the specific port"""
 
-        # TODO: fill in parameters
-        self.port_to[index].handle_packet("")
+        packet.source_name = self.name
+        self.port_to[index].handle_packet(packet)
 
 
 def create_link(node1: NetworkDevice, node2: NetworkDevice):
@@ -128,10 +237,10 @@ class Net:
         hosts: list[str],
         switches: list[str],
         links: list[tuple[str, str]],
-        ip_table: dict[str, Ip],
-        mac_table: dict[str, Mac],
+        ip_table: dict[str, IpAddress],
+        mac_table: dict[str, MacAddress],
     ):
-        self.host_dict = {
+        self.host_dict: dict[str, Host] = {
             host_name: Host(
                 name=host_name,
                 ip=ip_table[host_name],
@@ -140,9 +249,8 @@ class Net:
             for host_name in hosts
         }
 
-        self.switch_dict = {
-            switch_name: Switch(name=switch_name, num_ports=3)
-            for switch_name in switches
+        self.switch_dict: dict[str, Switch] = {
+            switch_name: Switch(name=switch_name) for switch_name in switches
         }
 
         for (name1, name2) in links:
@@ -163,11 +271,12 @@ class Net:
     def from_setting(cls):
         hostlist = setting.get_hosts().split()
         switchlist = setting.get_switches().split()
-        link_command = setting.get_links()
+        link_command = setting.get_links().split()
         ip_table = setting.get_ip()
         mac_table = setting.get_mac()
 
         links = [tuple(link.split(",")) for link in link_command]
+        logging.debug(links)
 
         return cls(
             hosts=hostlist,
@@ -204,12 +313,13 @@ class Net:
     def ping(self, node1: str, node2: str):
         """Initiate a ping between two hosts"""
 
-        if node1 not in self.host_dict or node2 not in self.host_dict:
+        host1 = self.host_dict.get(node1)
+        host2 = self.host_dict.get(node2)
+
+        if host1 is None or host2 is None:
             logging.error(f"Invalid hosts: {node1}, {node2}")
             return
 
-        host1 = self.host_dict[node1]
-        host2 = self.host_dict[node2]
         host1.ping(host2.ip)
 
     def show_table(self, target: str):
@@ -236,6 +346,7 @@ class Net:
 
 
 def main():
+    logging.basicConfig(level=logging.DEBUG)
     net = Net.from_setting()
     net.run()
 
