7cd62c70d26e0766f3491a1c9c349b18feb757e2
alan910127 <alan469357@gmail.com>
2023-06-05 17:17:11 +0800

    ✨ Add network topologies

---
+++ hw7/src/topologies.py
@@ -0,0 +1,25 @@
+from mininet.topo import Topo
+
+
+class NetworkOneTopology(Topo):
+    def build(self):
+        hosts = [self.addHost(f"h{i}") for i in range(1, 5)]
+        switch = self.addSwitch("s1")
+
+        for host in hosts:
+            self.addLink(host, switch)
+
+
+class NetworkTwoTopology(Topo):
+    def build(self):
+        hosts = [self.addHost(f"h{i}") for i in range(5, 9)]
+        switch = self.addSwitch("s2")
+
+        for host in hosts:
+            self.addLink(host, switch)
+
+
+topos = {
+    "n1topo": NetworkOneTopology,
+    "n2topo": NetworkTwoTopology,
+}
